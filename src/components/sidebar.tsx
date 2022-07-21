083b0aacb723ce67ad1f049e8484bc26539f7ac3
Mohan <mohanavel15@protonmail.com>
2022-07-21 12:06:56 +0530

    ui fixes

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,11 +1,10 @@
-import React from 'react'
 import ChannelList from './sidebar/channel_list'
 import Profile from './sidebar/profile'
 import Search from './sidebar/search'
 
 export default function SideBar() {
   return (
-    <div className='flex flex-col w-60 min-h-full bg-gray-700'>
+    <div className='flex flex-col w-60 min-h-full max-h-screen bg-gray-700'>
         <Search />
         <ChannelList />
         <Profile />
